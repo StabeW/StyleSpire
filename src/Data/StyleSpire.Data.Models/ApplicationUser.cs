3829ec6071506c78548d756ed75d7d4595e9b5af
deluxc <136242540+StabeW@users.noreply.github.com>
2024-04-25 11:17:26 +0300

    Create Web Api

---
+++ src/Data/StyleSpire.Data.Models/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿// ReSharper disable VirtualMemberCallInConstructor
+namespace StyleSpire.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Identity;
+    using StyleSpire.Data.Common.Models;
+
+    public class ApplicationUser : IdentityUser, IAuditInfo, IDeletableEntity
+    {
+        public override string Id { get; set; } = Guid.NewGuid().ToString();
+
+        // Audit info
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        // Deletable entity
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public virtual ICollection<IdentityUserRole<string>> Roles { get; set; } = new HashSet<IdentityUserRole<string>>();
+
+        public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; } = new HashSet<IdentityUserClaim<string>>();
+
+        public virtual ICollection<IdentityUserLogin<string>> Logins { get; set; } = new HashSet<IdentityUserLogin<string>>();
+    }
+}
---
+++ src/StyleSpire.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace StyleSpire.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "StyleSpire";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
