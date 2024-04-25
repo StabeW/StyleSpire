141112e1a0278fb3b41a2e18e2783227b4fe78be
deluxc <136242540+StabeW@users.noreply.github.com>
2024-04-25 17:42:37 +0300

    Create entity models

---
+++ src/Data/StyleSpire.Data.Models/Address.cs
@@ -0,0 +1,19 @@
+﻿namespace StyleSpire.Data.Models
+{
+    using StyleSpire.Data.Common.Models;
+
+    public class Address : BaseDeletableModel<Address>
+    {
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public string Country { get; set; }
+
+        public string Town { get; set; }
+
+        public string Street { get; set; }
+
+        public int PostCode { get; set; }
+    }
+}
---
+++ src/Data/StyleSpire.Data.Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace StyleSpire.Data.Models
+{
+    using System.Collections.Generic;
+
+    using StyleSpire.Data.Common.Models;
+
+    public class Order : BaseDeletableModel<Order>
+    {
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public ICollection<OrderItem> OrderItem { get; set; } = new HashSet<OrderItem>();
+
+        public decimal TotalPrice { get; set; }
+    }
+}
